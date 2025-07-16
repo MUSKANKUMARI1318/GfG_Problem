0146a1a3cdc75a74f1c61bc7974ad71e33080b57
Muskan Kumari <mk0008349@gmail.com>
2025-07-16 19:37:16 +0530

    Update pattern-20.py

--- Difficulty: Easy/Pattern 20/pattern-20.py
+++ Difficulty: Easy/Pattern 20/pattern-20.py
@@ -1,9 +1,9 @@
 class Solution:
     def printTriangle(self, n):
-        
-        
+        # Upper part
         for i in range(1, n + 1):
             print('*' * i + ' ' * (2 * (n - i)) + '*' * i)
-        
+
+        # Lower part
         for i in range(n - 1, 0, -1):
             print('*' * i + ' ' * (2 * (n - i)) + '*' * i)
