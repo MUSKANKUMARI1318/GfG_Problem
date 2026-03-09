20104b0f9bab36dad3c4b19e0bc23a0f741f70cf
Muskan Kumari <mk0008349@gmail.com>
2026-03-09 21:52:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String largestSwap(String s) {
+        // code here
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        
+       int maxIdx = n - 1;
+       int left = -1, right = -1;
+       
+       for(int i = n - 1; i>= 0; i--){
+           if(arr[i] > arr[maxIdx]){
+           
+           maxIdx = i;
+           
+       }
+       
+       else if(arr[i] < arr[maxIdx] ){
+           left = i;
+           right = maxIdx;
+       }
+        
+        
+        
+        
+
+    }
+    
+    if(left != -1){
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        
+    }
+    
+    return new String(arr);
+    
+  }
+}
